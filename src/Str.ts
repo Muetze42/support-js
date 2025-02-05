437e7b00aa6a558691bdf51a606fe0ee9c3fa2d5
Norman Huth <norman@huth.it>
2025-02-05 20:16:42 +0100

    feat: add uuid

--- src/Str.ts
+++ src/Str.ts
@@ -142,4 +142,13 @@ export class Str {
   public static numbers(value: string): string {
     return value.replace(/[^0-9]/g,'')
   }
+
+  /**
+   * Generate a UUID (version 4).
+   */
+  public static uuid(): string {
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+    )
+  }
 }
